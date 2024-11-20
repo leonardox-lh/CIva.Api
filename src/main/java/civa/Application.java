55d7fb14dcf040db7fa6c0a84b0d97a2432e396e
leonardo <u202124304@upc.edu.pe>
2024-11-20 03:27:32 -0500

    create project

---
+++ src/main/java/civa/Application.java
@@ -0,0 +1,25 @@
+package civa;
+
+import civa.shared.util.Utilities;
+import civa.user.model.enums.ERole;
+import civa.user.repository.IRoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initDatabase(IRoleRepository roleRepository) {
+        return args -> {
+            Utilities.insertRoleIfNotFound(roleRepository, ERole.ROLE_USER);
+            Utilities.insertRoleIfNotFound(roleRepository, ERole.ROLE_ADMIN);
+        };
+    }
+}
